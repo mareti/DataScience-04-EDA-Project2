d9cb4542a2ec6f48ac3ba535bc816e2db9f4976b
Muralidhar Areti <Muralidhar.Areti@outlook.com>
2015-06-20 20:31:54 -0400

    plot2 - final

---
+++ plot2.R
@@ -0,0 +1,32 @@
+# load required libraries
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+
+plot2 <- function() {
+    # initialize
+    startTime = now()
+    
+    # Load data
+    NEI = readRDS("./data/summarySCC_PM25.rds")
+    SCC = readRDS("./data/Source_Classification_Code.rds")
+    
+    x = NEI %>%
+        filter(fips=="24510") %>%
+        group_by(year) %>%
+        summarize(sumEmissions=sum(Emissions))
+    
+    png(filename="plot2.png", width=480, height=480, units="px")
+    barplot(
+        x$sumEmissions/(10^3)
+        , xlab = 'Year'
+        , ylab = expression(paste("PM", ""[2.5], "in thousands"))
+        , names.arg = x$year
+        , main = expression(paste("Emission of PM"[2.5], "in Baltimore City"))
+    )
+    dev.off()
+    
+    # Runtime
+    endTime = now()
+    print(paste("Runtime:", endTime-startTime, "seconds"))
+}
